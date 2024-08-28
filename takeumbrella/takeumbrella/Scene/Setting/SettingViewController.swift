08794d0686732929bee67b40622f3fcc7b9b36c1
iychoi <iychoi@direcionsoft.com>
2024-08-28 16:33:15 +0900

    SettingViewController 불필요 코드 제거

--- takeumbrella/takeumbrella/Scene/Setting/SettingViewController.swift
+++ takeumbrella/takeumbrella/Scene/Setting/SettingViewController.swift
@@ -8,22 +8,13 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    static func instance() -> SettingViewController? {
+        guard let vc = SettingViewController(nibName: "SettingViewController", bundle: nil) as? SettingViewController else { return nil }
+        return vc
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
