4b759ed55438fbf526f343f1b767e8ccfecc3115
iychoi <iychoi@direcionsoft.com>
2024-08-28 13:59:49 +0900

    프로젝트 파일 추가

---
+++ takeumbrella/takeumbrella/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  takeumbrella
+//
+//  Created by directionsoft on 8/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
