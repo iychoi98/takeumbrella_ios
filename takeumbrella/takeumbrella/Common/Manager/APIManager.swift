e6bae03d9a9f43ecea47fe233037bbbdadcdac1d
iychoi <iychoi@direcionsoft.com>
2024-08-29 09:39:07 +0900

    Manager 파일 추가 (APIManager, MapManager)

---
+++ takeumbrella/takeumbrella/Common/Manager/APIManager.swift
@@ -0,0 +1,12 @@
+//
+//  APIManager.swift
+//  takeumbrella
+//
+//  Created by directionsoft on 8/29/24.
+//
+
+import Foundation
+
+class APIManager: NSObject {
+    
+}
---
+++ takeumbrella/takeumbrella/Common/Manager/MapManager.swift
@@ -0,0 +1,12 @@
+//
+//  MapManager.swift
+//  takeumbrella
+//
+//  Created by directionsoft on 8/29/24.
+//
+
+import Foundation
+
+class MapManager: NSObject {
+    
+}
