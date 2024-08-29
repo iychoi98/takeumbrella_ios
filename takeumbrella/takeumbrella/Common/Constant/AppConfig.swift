1734ae8c35a19f13763ff65a3a017bd8e521b138
iychoi <iychoi@direcionsoft.com>
2024-08-29 18:01:54 +0900

    Constant 폴더 추가 (AppConfig, URLs)

---
+++ takeumbrella/takeumbrella/Common/Constant/AppConfig.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfig.swift
+//  takeumbrella
+//
+//  Created by directionsoft on 8/29/24.
+//
+
+import Foundation
+
+// MARK: 날씨 API
+let SERVICE_KEY_ENCODED = "Im1QAPVJ%2B941DtyBnhrt4Lyi4bPRTYgYPNXEa6vIqQjb9mnEYBQnqz1MQy0eikO5iOm1%2B47YIrwLBSYmYo76RA%3D%3D" // Check
+let SERVICE_KEY_DECODED = "Im1QAPVJ+941DtyBnhrt4Lyi4bPRTYgYPNXEa6vIqQjb9mnEYBQnqz1MQy0eikO5iOm1+47YIrwLBSYmYo76RA=="
+
+
+
+// Sample
+// http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=Im1QAPVJ%2B941DtyBnhrt4Lyi4bPRTYgYPNXEa6vIqQjb9mnEYBQnqz1MQy0eikO5iOm1%2B47YIrwLBSYmYo76RA%3D%3D&base_date=20210628&base_time=0600&nx=55&ny=127
---
+++ takeumbrella/takeumbrella/Common/Constant/URLs.swift
@@ -0,0 +1,23 @@
+//
+//  URLs.swift
+//  takeumbrella
+//
+//  Created by directionsoft on 8/29/24.
+//
+
+import Foundation
+
+
+struct URLs {
+    static let SERVICE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0"
+    
+    // 초단기예보
+    static let VERY_SHORT_TERM_FORECAST = "/getUltraSrtFcst"
+    
+    
+    
+    // 초단기예보조회 getUltraSrtFcst
+    // 예보시점부터 6시간 이내의 예보
+    // AWS?
+    // 난 강수량을 체크
+}
